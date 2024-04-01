56b5bbaabdff4bfc106322d6cae05b2f2f96bba6
elliewi11iams <155931022+elliewi11iams@users.noreply.github.com>
2024-04-01 16:18:06 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 
+/**
+ * Database.java
+ * 
+ * An interface that lists the methods that the Database uses
+ * 
+ * @author Jack Juncker, Gilbert Chang, Sahil Shetty
+ */
 public interface Database {
     public ArrayList<Object> readDatabase();
-    public boolean writeDatabase(); 
+    public boolean writeDatabase();
 }
