1c86cd2c3402f0521a96eb034f3eafd4b2657653
elliewi11iams <155931022+elliewi11iams@users.noreply.github.com>
2024-04-01 15:39:27 -0400

    Update ActionNotAllowedException.java

--- ActionNotAllowedException.java
+++ ActionNotAllowedException.java
@@ -1,14 +1,3 @@
-/*
-* ActionNotAllowedException
-*
-* An exception that is thrown when a forbidden action occurs.
-*
-* @author Ellie Williams
-*
-* @version March 29, 2024
-*
- */
-
 public class ActionNotAllowedException extends Exception {
     public ActionNotAllowedException(String message) {
         super(message);
